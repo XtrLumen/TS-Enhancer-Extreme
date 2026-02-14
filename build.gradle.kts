7a6a260e8f987b0b2c48c6989fb503b7aca24c28
XtrLumen <XtrLumen@outlook.com>
2026-02-14 23:07:42 +0800

    Remove CHANGELOG.md. someone else's WebUI; it will definitely be rewritten later.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ val verType by extra("")
 val verCode by extra(
     providers.exec {
         commandLine("git", "rev-list", "HEAD", "--count")
-    }.standardOutput.asText.get().trim().toInt() + 40
+    }.standardOutput.asText.get().trim().toInt() + 39
 )
 val verHash by extra(
     providers.exec {
